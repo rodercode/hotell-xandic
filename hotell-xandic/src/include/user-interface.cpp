51cf258043f6fc0ced55703f1f4616c95de62aa8
rodersnow <rodersnow@gmail.com>
2024-01-14 12:46:11 +0100

    Create booking menu A sub menu that has following options: - Add booking - Read all booking - Update booking - Delete booking

--- hotell-xandic/src/include/user-interface.cpp
+++ hotell-xandic/src/include/user-interface.cpp
@@ -9,6 +9,13 @@ void UserInterface::displayMainMenu(){
 	std::cout << "[3] Exit\n";
 }
 
+void UserInterface::displayBookingMenu() {
+	std::cout << "[1] Create booking";
+	std::cout << "[2] Read all booking";
+	std::cout << "[3] Update booking ";
+	std::cout << "[1] Delete booking";
+}
+
 void UserInterface::printMessage(string message) {
 	std::cout << message << '\n';
 }
--- hotell-xandic/src/include/user-interface.h
+++ hotell-xandic/src/include/user-interface.h
@@ -5,6 +5,10 @@ using std::string;
 
 class UserInterface {
 	public:
+		
+		
 		void displayMainMenu();
+		void displayBookingMenu();
+		void printMessage(string message);
 		void nextLine();
 };
